915c2ef57b7755bff87101da32bb2f7df2174611
mahjoubmahjoub <mahjoub.m@shazler.com>
2025-05-25 22:46:52 +0100

    fixe

--- ms_crm_hosgan_intervention/models/medicament.py
+++ ms_crm_hosgan_intervention/models/medicament.py
@@ -7,9 +7,9 @@ class Ordonnance(models.Model):
     _description = "Ordonnance"
     _order = 'name'
 
-    med_active = fields.Boolean("médicament active")
+    med_active = fields.Boolean("Médicament active")
     name = fields.Char(string="Nom du médicament",required=True)
-    number_day = fields.Integer(string="Nombre de fois par jour")
+    number_day = fields.Integer(string="Nombre de fois par jours")
     pendant = fields.Integer(string="Pendant")
     semaine_jour_mois = fields.Selection([('jour', 'Jour'),('semaine', 'Semaine'),('mois', 'Mois'),],string="Unité de temps" )
 
@@ -18,12 +18,12 @@ class Bilan(models.Model):
     _name = "bilan"
     _description = "Bilan"
     _order = 'categorie'
-    categorie = fields.Selection([
-        ('Bilan biologique', 'Bilan biologique'),
-        ('Examens radiologiques', 'Examens radiologiques'),
-        ('Examens cardiaques', 'Examens cardiaques'),
-        ('Bilan biologique chirurgie bariatrique', 'Bilan biologique chirurgie bariatrique'),
-    ], string="Type de Bilan")
 
+    categorie = fields.Selection([
+        ('BILAN BIOLOGIQUE', 'BILAN BIOLOGIQUE'),
+        ('EXAMENS RADIOLOGIQUES', 'EXAMENS RADIOLOGIQUES'),
+        ('EXAMENS CARDIAQUES', 'EXAMENS CARDIAQUES'),
+        ('BILAN BIOLOGIQUE CHIRURGIE BARIATRIQUE', 'BILAN BIOLOGIQUE CHIRURGIE BARIATRIQUE'),
+    ], string="Type de Bilan")
     name = fields.Char(string="Nom du Bilan",required=True)
-    med_active = fields.Boolean("médicament active")
+    med_active = fields.Boolean("Bilan active")
