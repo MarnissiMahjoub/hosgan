811931c9524a4bcc697c5c47a89bbb4c549865c7
mahjoubmahjoub <mahjoub.m@shazler.com>
2025-06-05 14:19:15 +0100

    add sm_hosgan_intervention_autorisation_report

--- sm_hosgan_intervention_autorisation_report/__manifest__.py
+++ sm_hosgan_intervention_autorisation_report/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "sm_hosgan_intervention_autorisation_report",
+    'name': "ms_hosgan_intervention_autorisation_report",
 
     'summary': "Short (1 phrase/line) summary of the module's purpose",
 
